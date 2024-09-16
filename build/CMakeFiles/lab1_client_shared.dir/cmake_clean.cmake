a842160021dfb54732665218d74c22aa7fc211b6
jiangyw <jiangyw@localhost>
2024-09-16 15:44:31 -0600

    init

---
+++ build/CMakeFiles/lab1_client_shared.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_client_shared.dir/lab1-client.c.o"
+  "lab1_client-shared"
+  "lab1_client-shared.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1_client_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
